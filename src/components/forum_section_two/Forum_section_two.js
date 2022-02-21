f9bb31a83bab1067755427ca400257cfdead3c2a
hannaheleen <schult.hannah@gmail.com>
2022-02-21 15:17:07 +0100

    Forum zwei Sections fürs Layout fertig gestellt

---
+++ src/components/forum_section_two/Forum_section_two.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./forum_section_two.css";
+
+export default class Forum_section_two extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <h4>hier kommt das Texteingabefeld hin</h4>
+        <div>
+          <h2>Headline</h2>
+          <p>Text</p>
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Forum_section_one from "../components/forum_section_one/Forum_section_one";
-// import Forum_section_two from "./components/forum_section_two/Forum_section_two";
+import Forum_section_two from "../components/forum_section_two/Forum_section_two";
 
 export default function Forum() {
-  return <Forum_section_one />;
+  return (
+    <>
+      <Forum_section_one />
+      <Forum_section_two />;
+    </>
+  );
 }
