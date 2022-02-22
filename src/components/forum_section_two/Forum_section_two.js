d3404d5563eabed0bd0246673b056f9e008e04d0
hannaheleen <schult.hannah@gmail.com>
2022-02-22 13:12:29 +0100

    Texteingabefeld mit Button ergänzt

--- src/components/forum_section_two/Forum_section_two.js
+++ src/components/forum_section_two/Forum_section_two.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import "./forum_section_two.css";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
 
 export default class Forum_section_two extends Component {
   render() {
     return (
       <div className="footer">
-        <h4>hier kommt das Texteingabefeld hin</h4>
-        <div>
-          <h2>Headline</h2>
-          <p>Text</p>
-        </div>
+        {/* <h4>hier kommt das Texteingabefeld hin</h4> */}
+        <>
+          <InputGroup>
+            <FormControl
+              placeholder="Schreib deinen Beitrag..."
+              aria-label="Recipient's username with two button addons"
+            />
+            <Button variant="outline-secondary">Hier klicken zum Posten</Button>
+            {/* <Button variant="outline-secondary">Button</Button> */}
+          </InputGroup>
+        </>
       </div>
     );
   }
